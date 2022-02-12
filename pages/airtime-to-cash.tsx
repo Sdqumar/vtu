3380732af902853e689d18eb8498b79831ba0713
sdqumar <sdqumar09@gmail.com>
2022-02-12 21:39:12 +0100

    AirtimeCash

--- pages/airtime-to-cash.tsx
+++ pages/airtime-to-cash.tsx
@@ -1,7 +1,134 @@
-export default function AirtimeCash(){
-    return(
-        <>
-        <h1>Airtime Cash</h1>
-        </>
-    )
+import Input from "../components/global/Input";
+import { useState } from "react";
+import Button from "../components/global/Button";
+import Select from "../components/global/select";
+import DataSelect from "../components/global/dataSelect";
+import { prices } from "../components/Home/utils";
+import { useForm } from "react-hook-form";
+
+
+export default function AirtimeCash() {
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [bundle, setBundle] = useState(prices[0].prices);
+
+  type form = {
+    network?: string;
+    PhoneNumber?: number;
+    amount?: number;
+    pin?: number;
+  };
+
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<form>();
+  const network = ["MTN", "Airtel", "9mobile", "GLO"];
+
+
+
+  const submitForm = (values: form) => {};
+
+  const handleShowForm = () => {
+    showForm ? setShowForm(false) : setShowForm(true);
+  };
+
+  
+  return (
+    <div className=" md:ml-20 mb-40   mt-10  ">
+      <section className="text-3xl ml-4 my-5  font-bold text-gray-800">
+       Convert Airtime to Cash
+      </section>
+      <main className="flex  flex-wrap">
+        <form
+          onSubmit={handleSubmit((formValues) => submitForm(formValues))}
+          className="transition-all duration-700 w-96 shadow-lg rounded-md p-8"
+        >
+          <Select
+            register={register}
+            name="network"
+            data={network}
+            label="Choose Network"
+            errors={errors}
+          />
+            <Input
+            register={register}
+            name="amount"
+            label="Amount"
+            type="number"
+            errors={errors}
+          />
+
+          <Input
+            register={register}
+            name="PhoneNumber"
+            label="Phone Number"
+            type="number"
+            errors={errors}
+          />
+         
+
+        
+          <Input
+            register={register}
+            name="pin"
+            label="PIN"
+            type="password"
+            errors={errors}
+          />
+          <Button label="continue" loading={loading} />
+        </form>
+        <section>
+          <button className="w-40 ml-10" onClick={handleShowForm}>
+            Save to Beneficiary
+          </button>
+          {showForm && <BeneficiaryForm />}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export function BeneficiaryForm() {
+  const [loading, setLoading] = useState(false);
+  type form = {
+    PhoneNumber?: number;
+    name?: string;
+  };
+
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    formState: { errors },
+  } = useForm<form>();
+  const submitForm = (values: form) => {};
+
+  return (
+    <main className="  mt-5 ">
+      <form
+        onSubmit={handleSubmit((formValues) => submitForm(formValues))}
+        className="transition-all duration-700 w-96 shadow-lg rounded-md p-8"
+      >
+        <Input
+          register={register}
+          name="PhoneNumber"
+          label="Phone Number"
+          type="number"
+          errors={errors}
+        />
+        <Input
+          register={register}
+          name="name"
+          label="Name"
+          type="name"
+          errors={errors}
+        />
+        <Button label="Save" loading={loading} />
+      </form>
+    </main>
+  );
 }
