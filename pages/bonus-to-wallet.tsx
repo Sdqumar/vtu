60056682ad2fce7bd72693eafaba0d52653b8015
sdqumar <sdqumar09@gmail.com>
2022-02-15 06:40:18 +0100

    BonusWallet

--- pages/bonus-to-wallet.tsx
+++ pages/bonus-to-wallet.tsx
@@ -1,7 +1,56 @@
-export default function BonusWallet(){
-    return(
-        <>
-        <h1>Bonus Wallet</h1>
-        </>
-    )
+import Input from "../components/global/Input";
+import { useState } from "react";
+import Button from "../components/global/Button";
+import Select from "../components/global/select";
+import { prices } from "../components/Home/utils";
+import { useForm } from "react-hook-form";
+
+export default function BonusWallet() {
+  const [loading, setLoading] = useState(false);
+
+  type form = {
+    amount?: number;
+    pin?: number;
+  };
+
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<form>();
+
+  const submitForm = (values: form) => {};
+
+  
+
+  return (
+    <div className=" md:ml-20 mb-40  mt-10  ">
+      <section className="text-xl ml-4 my-5  font-bold text-gray-800 ">
+      Move Earnings to Wallet (Agents Only)
+      </section>
+      <main className="flex  flex-wrap">
+        <form
+          onSubmit={handleSubmit((formValues) => submitForm(formValues))}
+          className="transition-all duration-700 w-96 shadow-lg rounded-md p-8"
+        >
+          <Input
+            register={register}
+            name="amount"
+            label="Amount"
+            type="number"
+            errors={errors}
+          />
+          <Input
+            register={register}
+            name="pin"
+            label="PIN"
+            type="password"
+            errors={errors}
+          />
+          <Button label="continue" loading={loading} />
+        </form>
+      </main>
+    </div>
+  );
 }
