3a64ad0f2a2c0c9ca9550684dd727c4c86cc9b68
sdqumar <sdqumar09@gmail.com>
2022-03-16 20:37:48 +0100

    feat: validate phone number

--- pages/buy-data.tsx
+++ pages/buy-data.tsx
@@ -55,6 +55,69 @@ export default function BuyData() {
   }, [watchBundle]);
 
   const submitForm = async (values: form) => {
+    const numberPrefix = values.phoneNumber?.toString().slice(0, 4);
+    const MTNPrefixes = [
+      "0806",
+      "0803",
+      "0704",
+      "0706",
+      "0906",
+      "0702",
+      "0814",
+      "0810",
+      "0813",
+      "0816",
+      "0903",
+      "0703",
+      "0913",
+    ];
+    const AirtelPrefixes = [
+      "0901",
+      "0808",
+      "0907",
+      "0812",
+      "0708",
+      "0701",
+      "0902",
+      "0802",
+    ];
+    const GloPrefixes = ["0905", "0805", "0705", "0815", "0807", "0811"];
+    const EtisaltPrefixes = ["0909", "0818", "0908", "0809", "0817"];
+
+    let isValidNumber;
+
+    if (values.network === "Airtel") {
+      isValidNumber = AirtelPrefixes.includes(numberPrefix!);
+    }
+
+    if (values.network === "GLO") {
+      isValidNumber = GloPrefixes.includes(numberPrefix!);
+    }
+
+    if (values.network === "9mobile") {
+      isValidNumber = EtisaltPrefixes.includes(numberPrefix!);
+    }
+
+    if (values.network === "MTN") {
+      isValidNumber = MTNPrefixes.includes(numberPrefix!);
+    }
+
+    if (values.phoneNumber?.toString().length !== 10) {
+      setError("phoneNumber", {
+        type: "number",
+        message: "Incorrect phone pumber!",
+      });
+      return;
+    }
+
+    if (!isValidNumber) {
+      setError("phoneNumber", {
+        type: "number",
+        message: `Incorrect ${values.network} phone pumber!`,
+      });
+      return;
+    }
+
     if (values.pin !== user.pin) {
       setError("pin", {
         type: "wrongpin",
@@ -133,7 +196,7 @@ export default function BuyData() {
             register={register}
             name="phoneNumber"
             label="Phone Number"
-            type="number"
+            maxLength={10}
             errors={errors}
           />
 
