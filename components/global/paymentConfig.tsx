f8de4796bf1c8017c30ddfba74fd3a271235d797
Sdqumar <sdqumar09@gmail.com>
2022-04-14 08:21:20 +0100

    feat: paystack live payment

--- components/global/paymentConfig.tsx
+++ components/global/paymentConfig.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
 import { usePaystackPayment } from "react-paystack";
+import { v4 as uuidv4 } from "uuid";
 
 const onClose = () => {
   console.log("closed");
@@ -23,13 +23,11 @@ const PaystackHook = ({ user, payment }: props) => {
     console.log(reference);
     router.push("/dashboard");
   };
-
   const config = {
-    reference: new Date().getTime().toString(),
+    reference: uuidv4(),
     email: user.email,
     amount: payment.total * 100,
-    // channels: ["bank"],
-    publicKey: "pk_test_cc89c527520c2442c1e462c3128f57442882a3ca",
+    publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PUBLICK_KEY!,
     metadata: {
       ...user,
       ...payment,
--- pages/fund.tsx
+++ pages/fund.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from "next";
 import Link from "next/link";
 
 const items = [
-  {
-    name: "ATM Card",
-    bg: "bg-green-400",
-    svg: "M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z",
-  },
+  // {
+  //   name: "ATM Card",
+  //   bg: "bg-green-400",
+  //   svg: "M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z",
+  // },
   {
     name: "Bank Transfer",
     bg: "bg-green-400",
