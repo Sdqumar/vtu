a847198dcf1eeffb62cc192929e896250312a6af
Sdqumar <sdqumar09@gmail.com>
2022-04-20 23:22:55 +0100

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import UserProvider from "../components/context/userContext";
 import Auth from "../components/global/Auth";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
 const Whatsapp = dynamic(() => import("../components/global/Whatapp"));
 const Sidebar = dynamic(() => import("../components/global/sidebar"));
@@ -21,6 +22,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           {!isHomepage && (
             <div className=" fixed z-10 h-12 w-full bg-white bg-opacity-60 backdrop-blur-lg backdrop-filter"></div>
           )}
+          <Head>
+            <link rel="icon" href="/favicon.png" />
+          </Head>
           <Component {...pageProps} />
         </div>
       </Auth>
